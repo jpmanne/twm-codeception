e6cf0467abf5984a90c9c739b8e61ee3295caa79
Testing <poornima.pr32@gmail.com>
2015-10-21 13:26:37 +0530

    Changed SmokeTest to test UI
    Changed SmokeTest to test UI

--- tests/acceptance/SmokeCept.php
+++ tests/acceptance/SmokeCept.php
@@ -1,23 +1,14 @@
-<?php 
-use Step\Acceptance\Login as login;
-use Step\Acceptance\ThriveTips as thriveTips;
-use Step\Acceptance\PublicMessages as homeWall;
-
+<?php
 //Login Functionality
-$I = new login($scenario);
-$I->login('researchadmin', 'AttainED%6');
-
-//Post as a Public Message
-$I = new homeWall($scenario);
-$I->public_Wall('Posting a Image', 'Add Photo', 'photo', 'Winter.jpg', '');
-//$I->public_Wall('Posting a Video', 'Share Video', 'video', '', 'https://www.youtube.com/watch?v=Bi-v6M4fGbA');
-
-//Thrivetips Functionality 
-/*$I = new thriveTips($scenario);
-$I->addContent('Add Html Content3', 'Html', 'html', 'The author, producer or publisher of an original source.', '', '', '', '', '', '', 'Medium', 'Thrive Tips Add Html Content3 has been created.');
-$I->addContent('Sample Video', 'Video', 'video', 'The author, producer or publisher of an original source.', 'https://www.youtube.com/watch?v=ZaxGcgUiBOo', '', '', '', '', 'Medium', 'Thrive Tips Sample Video has been created.');
-$I->addContent('Sample OffsiteContent', 'Offsite Content', 'offsite', '', '', '', '', 'Offsite Content', '', '#edit-field-thrive-tags-und-3', 'Thrive Tips Sample OffsiteContent has been created.');
-$I->addContent('Sample PDF', 'Pdf', 'pdf', '', '', '', 'Ticket.pdf', '#edit-field-thrive-tags-und-2', 'Thrive Tips Sample PDF has been created.');*/
-
-//Clicking on Logout  button
-//$I->click('logout');
+$I = new AcceptanceTester($scenario);
+$I->amOnPage('/'); // validates the application URL from acceptance.suite.yml file
+$I->wantTo('Ensure the SIGN IN functionality');
+$I->canSeeInCurrentUrl('stage.twm.radiantexp.com');// conditional assertions to check specific URL is visible or not
+$I->fillField('#user-login-form input[name=name]', 'researchadmin'); // Enters username textfield using 'name' locator 
+$I->fillField('#user-login-form input[name=pass]', 'AttainED%6'); // Enters password textfield  using 'name' locator 
+$I->click('Log in'); // Clicks on  Log in button
+$I->dontsee('Username field is required.'); // Assertion statement to check Error message is visible on page or not
+$I->dontsee('Password field is required.'); // Assertion statement to check Error message is visible on page or not
+$I->see('Home'); // Assertion statement to check message is visible on page or not	
+$I->see('Log out');	//Log out Functionality // Assertion statement to check Logout message is visible on page or not
+$I->click('Log out');	//Log out Functionality // Clicks Logout button
