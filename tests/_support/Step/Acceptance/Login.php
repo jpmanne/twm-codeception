aa6d456e7a95926a8de6bb194caa6ecce41d4668
jpmanne <jayaprakash.manne@sciits.com>
2015-12-08 17:51:19 +0530

    Adding assertions
    Adding assertions

--- tests/_support/Step/Acceptance/Login.php
+++ tests/_support/Step/Acceptance/Login.php
@@ -6,10 +6,18 @@ class Login extends \AcceptanceTester
     public function login($user, $pass)
     {
         $I = $this;
+		$I->amGoingTo('LOGIN WITH VALID CREDENTIALS');
         $I->amOnPage('/');
         $I->fillField('#user-login-form input[name=name]', $user);
         $I->fillField('#user-login-form input[name=pass]', $pass);
         $I->click('Log in');
-		$I->dontsee('Password field is required.');
+		$message = $I->grabTextFrom('#page-title');
+		if ($message == 'Home') {
+			$I->expect('SIGN-IN IS SUCCESSFUL');
+		} else if($message != 'Home') {
+			$I->expect('SIGN-IN IS UNSUCCESSFUL');
+			$I->see('Username field is required.');
+			$I->see('Password field is required.');
+		}
     }
 }
