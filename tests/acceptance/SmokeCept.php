dba691c4d362abfac5b7cf308540b31155426229
jpmanne <jayaprakash.manne@sciits.com>
2015-12-08 18:27:18 +0530

    Adding input data
    Adding input data

--- tests/acceptance/SmokeCept.php
+++ tests/acceptance/SmokeCept.php
@@ -2,27 +2,26 @@
 use Step\Acceptance\Login as login;
 use Step\Acceptance\ThriveTips as thriveTips;
 use Step\Acceptance\PublicMessages as homeWall;
+use Step\Acceptance\Achievements as user_achvmts;
+use Step\Acceptance\Surveydetails as survey;
 use Step\Acceptance\Logout as logout;
 
 //Login Functionality
 $I = new login($scenario);
-$I->amGoingTo('LOGIN WITH VALID CREDENTIALS');
-$I->login('researchadmin', 'AttainED%6');
-//$I->login('participant', '07selEna+?');
-$I->expect('SIGN-IN IS SUCCESSFUL');
+$I->login('rauser2', 'rauser');
+//$I->login('researchadmin', 'AttainED%6');
+
+//Thrivetips Functionality 
+$I = new thriveTips($scenario);
+$I->addContent('Selenium Video', 'Video', 'video', '', 'Selenium Training video', 'https://www.youtube.com/watch?v=IhJgrLjljpc', '', '', '', '', 'Medium', '8', 'Thrive Tips Selenium Video has been created.');
 
 //Post as a Public Message
 $I = new homeWall($scenario);
-$I->amGoingTo('POST PUBLIC MESSAGE OF IMAGE CATEGORY');
-$I->public_Wall('Image', 'Add Photo', 'photo', 'RunAppDownload.jpg', '', 'RA', 'Cool', 'html/body/div[2]/main/div/fieldset/div/div[2]/div[3]/div[3]/span/a', 'html/body/div[2]/main/div/fieldset/div/div[2]/div[3]/div[3]/span/a', 'html/body/div[2]/main/div/fieldset/div/div[2]/div[3]/div[4]/span/a', 'html/body/div[2]/main/div/fieldset/div/div[2]/div[3]/div[4]/span/a', '', '', '', '', '');
-$I->expect('PUBLIC MESSAGE POSTED SUCCESSFULLY');
+$I->public_Wall('Image', 'Add Photo', 'photo', 'Winter.jpg', '', 'RA', 'Cool', 'html/body/div[2]/main/div/fieldset/div/div[1]/div[3]/div[2]/span/a', '', 'html/body/div[2]/main/div/fieldset/div/div[1]/div[3]/div[5]/div[2]/div[2]/span[2]/span/a', '', '', '', '', '', '');
 
-/*//Thrivetips Functionality 
-$I = new thriveTips($scenario);
-$I->addContent('Add Html Content3', 'Html', 'html', 'The author, producer or publisher of an original source.', '', '', '', '', '', '', 'Medium', 'Thrive Tips Add Html Content3 has been created.');
-$I->addContent('Sample Video', 'Video', 'video', 'The author, producer or publisher of an original source.', 'https://www.youtube.com/watch?v=ZaxGcgUiBOo', '', '', '', '', 'Medium', 'Thrive Tips Sample Video has been created.');
-$I->addContent('Sample OffsiteContent', 'Offsite Content', 'offsite', '', '', '', '', 'Offsite Content', '', '#edit-field-thrive-tags-und-3', 'Thrive Tips Sample OffsiteContent has been created.');
-$I->addContent('Sample PDF', 'Pdf', 'pdf', '', '', '', 'Ticket.pdf', '#edit-field-thrive-tags-und-2', 'Thrive Tips Sample PDF has been created.');*/
+//Check Posts and Votes Achievements to be Unlocked
+$I = new user_achvmts($scenario);
+$I->achievement('html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div[1]', 'html/body/div[2]/main/div/div[2]/div/div[2]/div[2]/div[2]/div[1]', 'html/body/div[2]/main/div/fieldset/div/div[3]/div[3]/div[2]/span/a', 'html/body/div[2]/main/div/fieldset/div/div[4]/div[3]/div[2]/span/a', 'html/body/div[2]/main/div/fieldset/div/div[5]/div[3]/div[2]/span/a', '2015/12/08');
 
 //Clicking on Logout button
 $I = new logout($scenario);
